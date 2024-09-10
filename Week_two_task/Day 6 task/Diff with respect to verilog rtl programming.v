22ce23e92d914c990ec27f15f52299dd12116bbb
sribalaji-16 <sribalajimuthuraman@outlook.com>
2024-09-10 22:15:12 +0530

    Update Diff with respect to verilog rtl programming.v

--- Week_two_task/Day 6 task/Diff with respect to verilog rtl programming.v
+++ Week_two_task/Day 6 task/Diff with respect to verilog rtl programming.v
@@ -22,7 +22,8 @@ Latches are used in asynchronous circuits.
 Speed and Power: 
 Latches are faster and consume less power.
 
-Usage: Commonly used for temporary data storage.
+Usage: 
+Commonly used for temporary data storage.
 
     
 Flip-Flops in Verilog RTL Programming:

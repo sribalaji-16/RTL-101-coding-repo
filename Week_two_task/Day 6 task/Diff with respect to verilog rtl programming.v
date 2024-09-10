bacaef315789a7e70e701f0aa25df148a3e4ba36
sribalaji-16 <sribalajimuthuraman@outlook.com>
2024-09-10 22:14:08 +0530

    Update Diff with respect to verilog rtl programming.v

--- Week_two_task/Day 6 task/Diff with respect to verilog rtl programming.v
+++ Week_two_task/Day 6 task/Diff with respect to verilog rtl programming.v
@@ -13,7 +13,9 @@ always @ (D or enable) begin
 end
 endmodule
 
-Control Signal: Latches use an enable signal instead of a clock signal.
+Control Signal: 
+Latches use an enable signal instead of a clock signal.
+
 Operation Type: Latches are used in asynchronous circuits.
 Speed and Power: Latches are faster and consume less power.
 Usage: Commonly used for temporary data storage.

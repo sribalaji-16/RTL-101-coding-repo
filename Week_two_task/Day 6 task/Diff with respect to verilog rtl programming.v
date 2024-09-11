fd5445691f83809065038aa941446fe5913f7285
sribalaji-16 <sribalajimuthuraman@outlook.com>
2024-09-11 23:39:39 +0530

    Update Diff with respect to verilog rtl programming.v

--- Week_two_task/Day 6 task/Diff with respect to verilog rtl programming.v
+++ Week_two_task/Day 6 task/Diff with respect to verilog rtl programming.v
@@ -26,7 +26,7 @@ Usage:
 Commonly used for temporary data storage.
 
     
-Flip-Flops in Verilog RTL Programming:
+Flip-Flops in Verilog RTL Programming: 
     
 Triggering Mechanism: Flip-flops are edge-sensitive. They change their output only on the rising or falling edge of a clock signal
     
